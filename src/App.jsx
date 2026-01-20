8eda1f43f12acdf0511234f97c7840226b1d67ae
aswin <aswinraj.stackup@gmail.com>
2026-01-20 13:16:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,26 @@ import Blinds from './pages/Blinds';
 import Blogs from './pages/blogs';
 import Blog from './pages/blog';
 import Gallery from './pages/gallery';
+import NotFoundPage from './pages/notfound';
 
 // import UnderMaintenance from './pages/underMaintenance'
 import './App.css';
 
 export default function App() {
   return (
-    <Router>
+      <Router>
       <Routes>
         {/* <Route path="/" element={<UnderMaintenance />} /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/curtains" element={<Curtains />} />
-        <Route path="/products/blinds" element={<Blinds />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/blog" element={<Blog />} />
-        <Route path="/gallery" element={<Gallery />} />
+         <Route path="/" element={<Home />} />
+         <Route path="/products" element={<Products />} />
+         <Route path="/products/curtains" element={<Curtains />} />
+         <Route path="/products/blinds" element={<Blinds />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/contact" element={<Contact />} />
+         <Route path="/blogs" element={<Blogs />} />
+         <Route path="/blogs/blog" element={<Blog />} />
+         <Route path="/gallery" element={<Gallery />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
