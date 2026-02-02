7d0b155fb284b56462f4c1238de1c65e5cbb892b
aswin <aswinraj.stackup@gmail.com>
2026-02-03 01:51:41 +0530

    first commit

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -8,11 +8,22 @@ function App() {
     const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
     const imageIds = [
-        "11xUC0bqBtuBkNegWpHaEwLT8uI4t-iK7",
-        "14W9P1WAN3cbkxXYXMzS6iMJVYpB3YApR",
-        "1FX5uLWQjsB_WXJ7ED4m-7pa23SHIPgxt",
-        "1GFyEho0_BhaNLIKqZ_6F-B1QVfDoHBMB",
-        "1HtgpYgMvMWSlCTAgDbYouBxfPtIa5yJZ",
+        "11EQb6S00dfr2P1NdhINxFzVkqj5GAmen",
+        "13CNrdk_4jXYnafvng-30PExT1b_30wES",
+        "13vuA23k6KkYPHVlxhxMQIZq02Qb-nqtK",
+        "16p4xbrZn-1VyD6qLCMHNe0m0oElpUX-i",
+        "18hJmUTla-V_Bf0pDrQi_POFlIIGWKvqo",
+        "1A6mbQyoClO7GLLz4oAsfA_J4_wDgIRm0",
+        "1QeRdXrMn0uWAMZtJssO_WA3A_c_2WDsh",
+        "1Ua5Cx_UupgX2GZqoJYGIMOtRLyUJqxkp",
+        "1aswy8SP-WnHAzr7iZUqEUgAbZtt1Ysya",
+        "1bMf0AduCAOwG4XVaCQ_hmxIJ9vRpn0l_",
+        "1bRt6biVmMp4S-qp0rzTAsxmY2FD8x-NJ",
+        "1dG0jqd5wDadEj3tusQpxb4Fb51Z95a-Q",
+        "1o4ebJGHwFcdmfJfD7uyx_9lvxFlfuDMR",
+        "1qe6OnjIJvveBeeECVhZ9BjzMR2hgXa5v",
+        "1vWCJ37cZ8ENg04Z1OrkQ9RLit57pAS75",
+        "1yE1xD9ZKT3pVWQClANM7YBoNfuOUEO9r",
         "1K2DHxdH0rDGRB13Qjvn2xklw3k7otfiM",
         "1KKFiv3STDJQrVVU6kkdasAhbEoHpqW1M",
         "1Lo9Fu7Lcm94YmbzcYK8SHtHKGLdNDwZ3",
@@ -24,11 +35,8 @@ function App() {
         "1on-vjubNiE8bO_zIWcdQ3SHLFbg10VW5",
         "1pSS1jm3wq0FMHO6X9WD2vjycFLh9pfjE",
         "1q78z7pCU1CzdKZx6MFksAAWbbCJSGr22",
-        "1q8u4p_4FydvgRvYQAmjc-qbF8soxlSlU",
         "1x7yjFp1Um6kPsWjEFyWlRgoumLmyLnRP",
-        "1zZJbCnZYVAXC573agvwg83tZrKjE9W8a"
     ];
-
 
     const openLightbox = (index) => {
         setSelectedImageIndex(index);
@@ -36,8 +44,8 @@ function App() {
     };
 
     const images = imageIds.map(
-  id => `https://drive.google.com/thumbnail?id=${id}&sz=w2000`
-);
+        id => `https://drive.google.com/thumbnail?id=${id}&sz=w2000`
+    );
 
 
     return (
